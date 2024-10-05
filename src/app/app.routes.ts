6a1955f1b5b617f1e3f8c212e3e5f3c8a2e8d54e
undcybele <andreea1bolea@gmail.com>
2024-10-05 11:16:07 +0300

    init setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {RegistrationComponent} from "./screens/registration/registration.component";
+import {ChatComponent} from "./screens/chat/chat.component";
+import {AuthGuard} from "./auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: RegistrationComponent},
+    {path: 'chat', component: ChatComponent, canActivate: [AuthGuard]},
+    {path: '**', redirectTo: ''}
+];
